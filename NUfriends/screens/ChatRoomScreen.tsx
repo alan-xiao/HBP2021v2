2a493ef189a4de3d5f9ebc8dd27863ba08bf2600
alanx <alanx99@gmail.com>
2021-02-25 19:23:25 -0500

    subscriptions kinda working

--- NUfriends/screens/ChatRoomScreen.tsx
+++ NUfriends/screens/ChatRoomScreen.tsx
@@ -1,37 +1,40 @@
 import React, { useEffect, useState } from "react";
 import {
-  View,
-  Text,
   FlatList,
+  Text,
   ImageBackground,
-  StyleSheet,
+  KeyboardAvoidingView,
 } from "react-native";
 
-import { API, graphqlOperation, Auth } from "aws-amplify";
-import { messagesByChatRoom } from "../graphql/queries";
-
 import { useRoute } from "@react-navigation/native";
+import { API, graphqlOperation, Auth } from "aws-amplify";
+
+import { messagesByChatRoom } from "../graphql/queries";
+import { onCreateMessage } from "../graphql/subscriptions";
+
 import ChatMessage from "../components/ChatMessage";
-import InputBox from "../components/InputBox";
 import BG from "../assets/images/BG.png";
+import InputBox from "../components/InputBox";
 
 const ChatRoomScreen = () => {
   const [messages, setMessages] = useState([]);
   const [myId, setMyId] = useState(null);
+
   const route = useRoute();
 
-  console.log(route.params.id);
-  useEffect(() => {
-    const fetchMessages = async () => {
-      const messagesData = await API.graphql(
-        graphqlOperation(messagesByChatRoom, {
-          chatRoomID: route.params.id,
-          sortDirection: "DESC",
-        })
-      );
+  const fetchMessages = async () => {
+    const messagesData = await API.graphql(
+      graphqlOperation(messagesByChatRoom, {
+        chatRoomID: route.params.id,
+        sortDirection: "DESC",
+      })
+    );
 
-      setMessages(messagesData.data.messagesByChatRoom.items);
-    };
+    console.log("FETCH MESSAGES");
+    setMessages(messagesData.data.messagesByChatRoom.items);
+  };
+
+  useEffect(() => {
     fetchMessages();
   }, []);
 
@@ -42,6 +45,28 @@ const ChatRoomScreen = () => {
     };
     getMyId();
   }, []);
+
+  useEffect(() => {
+    const subscription = API.graphql(
+      graphqlOperation(onCreateMessage)
+    ).subscribe({
+      next: (data) => {
+        const newMessage = data.value.data.onCreateMessage;
+
+        if (newMessage.chatRoomID !== route.params.id) {
+          console.log("Message is in another room!");
+          return;
+        }
+
+        fetchMessages();
+        // setMessages([newMessage, ...messages]);
+      },
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  console.log(`messages in state: ${messages.length}`);
 
   return (
     <ImageBackground style={{ width: "100%", height: "100%" }} source={BG}>
